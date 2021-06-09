d6191903e850856a29fa52583dc8595149fd54f9
甘大业 <tiga.gan@aishu.cn>
2021-06-09 14:29:16 +0800

    [feat](test) add benchmark script

---
+++ devops/CI/benchmark.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+## Please ensure run this script after  login docker regitry
+set -e 
+source get_env.sh
+
+BuildImage="golang:1.15"
+
+function UT(){
+  echo "|------UT-------|"
+  docker run --rm -v $CODESPACE/span:/root/go  $BuildImage sh -c "cd /root/go && sh -x benchtest.sh"
+}
