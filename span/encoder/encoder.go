cc9be08c7413938a99e63d26f96bc5bfac8f205d
甘大业 <tiga.gan@aishu.cn>
2021-06-21 17:09:09 +0800

    [fix](encoder) fix time convert

--- span/encoder/encoder.go
+++ span/encoder/encoder.go
@@ -129,7 +129,7 @@ func (js *JsonEncoder) write(f field.Field) error {
 		_, res := w.WriteBytes(_quotation)
 		return res
 	case field.TimeType:
-		v := strconv.FormatInt(time.Time(f.(field.TimeField)).Nanosecond(), 10)
+		v := strconv.FormatInt(int64(time.Time(f.(field.TimeField)).Nanosecond()), 10)
 		bytes := js.string2Bytes(v)
 		_, res := w.WriteBytes(bytes)
 		return res
