2fc1e1239edc6f92adc544cee728adc5ccc5b7bc
Rocky.luo <Rocky.luo@aishu.cn>
2022-09-26 13:32:11 +0800

    修改目录结构

--- exporters/artrace/examples/oneservice.go
+++ exporters/artrace/examples/oneservice.go
@@ -3,7 +3,7 @@ package examples
 import (
 	"context"
 	"crypto/tls"
-	"devops.aishu.cn/AISHUDevOps/ONE-Architecture/_git/TelemetrySDK-Go.git/exporters/artrace/internal/client"
+	"devops.aishu.cn/AISHUDevOps/ONE-Architecture/_git/TelemetrySDK-Go.git/exporters/artrace"
 	"devops.aishu.cn/AISHUDevOps/ONE-Architecture/_git/TelemetrySDK-Go.git/exporters/artrace/internal/config"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
@@ -19,7 +19,7 @@ const result = "the answer is"
 
 // add 计算两数之和。
 func add(ctx context.Context, x, y int64) (context.Context, int64) {
-	ctx, span := Tracer.Start(ctx, "加法", trace.WithSpanKind(1))
+	ctx, span := artrace.Tracer.Start(ctx, "加法", trace.WithSpanKind(1))
 	span.SetAttributes(attribute.KeyValue{Key: "add", Value: attribute.StringValue("计算两数之和")})
 	defer span.End()
 
@@ -30,7 +30,7 @@ func add(ctx context.Context, x, y int64) (context.Context, int64) {
 
 // multiply 计算两数之积。
 func multiply(ctx context.Context, x, y int64) (context.Context, int64) {
-	ctx, span := Tracer.Start(ctx, "乘法", trace.WithSpanKind(2), trace.WithLinks(trace.Link{}))
+	ctx, span := artrace.Tracer.Start(ctx, "乘法", trace.WithSpanKind(2), trace.WithLinks(trace.Link{}))
 	span.SetStatus(2, "成功计算乘积")
 	span.AddEvent("multiplyEvent")
 	span.SetAttributes(attribute.KeyValue{Key: "succeeded", Value: attribute.BoolValue(true)})
@@ -44,9 +44,9 @@ func multiply(ctx context.Context, x, y int64) (context.Context, int64) {
 // StdoutExample 输出到控制台和本地文件。
 func StdoutExample() {
 	ctx := context.Background()
-	c := NewStdoutClient()
-	exporter := NewExporter(c)
-	tracerProvider := sdktrace.NewTracerProvider(sdktrace.WithBatcher(exporter), sdktrace.WithResource(ServiceResource))
+	c := artrace.NewStdoutClient()
+	exporter := artrace.NewExporter(c)
+	tracerProvider := sdktrace.NewTracerProvider(sdktrace.WithBatcher(exporter), sdktrace.WithResource(artrace.ServiceResource))
 	otel.SetTracerProvider(tracerProvider)
 	defer func() {
 		if err := tracerProvider.Shutdown(ctx); err != nil {
@@ -64,10 +64,10 @@ func StdoutExample() {
 func HTTPExample() {
 	ctx := context.Background()
 	u, _ := url.Parse(strings.TrimSpace("http://10.4.130.68:880/api/feed_ingester/v1/jobs/traceTest/events"))
-	c := NewHTTPClient(config.WithScheme(u.Scheme), config.WithEndpoint(u.Host),
+	c := artrace.NewHTTPClient(config.WithScheme(u.Scheme), config.WithEndpoint(u.Host),
 		config.WithPath(u.Path), config.WithCompression(config.GzipCompression))
-	exporter := client.NewExporter(c)
-	tracerProvider := sdktrace.NewTracerProvider(sdktrace.WithBatcher(exporter), sdktrace.WithResource(ServiceResource))
+	exporter := artrace.NewExporter(c)
+	tracerProvider := sdktrace.NewTracerProvider(sdktrace.WithBatcher(exporter), sdktrace.WithResource(artrace.ServiceResource))
 	otel.SetTracerProvider(tracerProvider)
 	defer func() {
 		if err := tracerProvider.Shutdown(ctx); err != nil {
@@ -85,11 +85,11 @@ func HTTPExample() {
 func HTTPSExample() {
 	ctx := context.Background()
 	u, _ := url.Parse(strings.TrimSpace("https://10.4.107.107/api/feed_ingester/v1/jobs/job-a6d44f634e80d530/events"))
-	c := client.NewHTTPClient(config.WithScheme(u.Scheme), config.WithEndpoint(u.Host),
+	c := artrace.NewHTTPClient(config.WithScheme(u.Scheme), config.WithEndpoint(u.Host),
 		config.WithPath(u.Path), config.WithCompression(config.GzipCompression), config.WithTimeout(10*time.Second),
 		config.WithHeader(nil), config.WithTLSClientConfig(&tls.Config{InsecureSkipVerify: true}))
-	exporter := client.NewExporter(c)
-	tracerProvider := sdktrace.NewTracerProvider(sdktrace.WithBatcher(exporter), sdktrace.WithResource(ServiceResource))
+	exporter := artrace.NewExporter(c)
+	tracerProvider := sdktrace.NewTracerProvider(sdktrace.WithBatcher(exporter), sdktrace.WithResource(artrace.ServiceResource))
 	otel.SetTracerProvider(tracerProvider)
 	defer func() {
 		if err := tracerProvider.Shutdown(ctx); err != nil {
--- exporters/artrace/initialization.go
+++ exporters/artrace/initialization.go
@@ -1,4 +1,4 @@
-package examples
+package artrace
 
 import (
 	"context"
