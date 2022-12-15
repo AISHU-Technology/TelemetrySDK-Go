ca86eb060a48981affe038a010e95d3c6068288b
Nick.nie <Nick.nie@aishu.cn>
2022-12-15 13:55:48 +0800

    [feature]log exporter实现sdk内置接口

--- exporter/ar_span/span_exporter.go
+++ exporter/ar_span/span_exporter.go
@@ -2,47 +2,41 @@ package ar_span
 
 import (
 	"context"
-	"io"
 
 	"devops.aishu.cn/AISHUDevOps/ONE-Architecture/_git/TelemetrySDK-Go.git/exporter/public"
+	"devops.aishu.cn/AISHUDevOps/ONE-Architecture/_git/TelemetrySDK-Go.git/span/encoder"
 )
 
-var _ io.Writer = (*Exporter)(nil)
+var _ encoder.Exporter = (*Exporter)(nil)
 
 // Exporter 导出数据到AnyRobot Feed Ingester的 log 数据接收器。
 type Exporter struct {
-	*public.Exporter
+	publicExporter *public.Exporter
+	ctx            context.Context
+	cancelFunc     context.CancelFunc
 }
-
-// ExportEvents 批量发送 AnyRobotEvents 到AnyRobot Feed Ingester的 Event 数据接收器。
-// func (e *Exporter) ExportEvents(ctx context.Context, events []eventsdk.Event) error {
-// 	if len(events) == 0 {
-// 		return nil
-// 	}
-// 	file := bytes.NewBuffer([]byte{})
-// 	encoder := json.NewEncoder(file)
-// 	encoder.SetEscapeHTML(false)
-// 	encoder.SetIndent("", "\t")
-// 	if err := encoder.Encode(events); err != nil {
-// 		return err
-// 	}
-// 	return e.ExportData(ctx, file.Bytes())
-// }
 
 // NewExporter 创建已启动的Exporter。
 func NewExporter(c public.Client) *Exporter {
+	ctx, cancel := context.WithCancel(context.Background())
 	return &Exporter{
-		public.NewExporter(c),
+		publicExporter: public.NewExporter(c),
+		ctx:            ctx,
+		cancelFunc:     cancel,
 	}
 }
 
 // Write 批量发送 log 到AnyRobot Feed Ingester的 log 数据接收器。
 func (e *Exporter) Write(p []byte) (n int, err error) {
-	ctx := context.Background()
-	exportDataErr := e.ExportData(ctx, p)
+	exportDataErr := e.publicExporter.ExportData(e.ctx, p)
 	if err != nil {
 		return 0, exportDataErr
 	} else {
 		return len(p), nil
 	}
+}
+
+// 返回cancel函数供sdk结束时调用
+func (e *Exporter) GetCancelFunc() context.CancelFunc {
+	return e.GetCancelFunc()
 }
