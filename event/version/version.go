737092b413a86069bb421493218b46dc2fecb160
Nick.nie <Nick.nie@aishu.cn>
2023-01-10 17:38:33 +0800

    [feature]修改version

--- event/version/version.go
+++ event/version/version.go
@@ -1,5 +1,5 @@
 package version
 
 const EventInstrumentationName = "TelemetrySDK-Go/exporter/ar_event"
-const EventInstrumentationVersion = "v2.5.0"
+const EventInstrumentationVersion = "v2.6.0"
 const EventInstrumentationURL = "https://devops.aishu.cn/AISHUDevOps/ONE-Architecture/_git/TelemetrySDK-Go?path=/exporter/ar_event"
