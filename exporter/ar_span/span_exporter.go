2615fdeaa5ae39d74548aaede68da7856eafd8ea
Nick.nie <Nick.nie@aishu.cn>
2023-01-13 10:50:34 +0800

    [feature]补充ut

--- exporter/ar_span/span_exporter.go
+++ exporter/ar_span/span_exporter.go
@@ -7,21 +7,21 @@ import (
 	"devops.aishu.cn/AISHUDevOps/ONE-Architecture/_git/TelemetrySDK-Go.git/span/exporter"
 )
 
-var _ exporter.LogExporter = (*Exporter)(nil)
+var _ exporter.LogExporter = (*SpanExporter)(nil)
 
 // Exporter 导出数据到AnyRobot Feed Ingester的 log 数据接收器。
-type Exporter struct {
+type SpanExporter struct {
 	*public.Exporter
 }
 
 // NewExporter 创建已启动的Exporter。
-func NewExporter(c public.Client) *Exporter {
-	return &Exporter{
+func NewExporter(c public.Client) *SpanExporter {
+	return &SpanExporter{
 		public.NewExporter(c),
 	}
 }
 
 // ExportLogs 批量发送 log 到AnyRobot Feed Ingester的 log 数据接收器。
-func (e *Exporter) ExportLogs(ctx context.Context, p []byte) error {
+func (e *SpanExporter) ExportLogs(ctx context.Context, p []byte) error {
 	return e.ExportData(ctx, p)
 }
