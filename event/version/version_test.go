a754b6492e260b65c1f006411bf38f5821490566
Rocky.luo <Rocky.luo@aishu.cn>
2023-07-13 15:06:41 +0800

    [changed]更新版本号

--- event/version/version_test.go
+++ event/version/version_test.go
@@ -7,6 +7,6 @@ import (
 
 func TestVersion(t *testing.T) {
 	convey.Convey("TestVersion", t, func() {
-		convey.So(EventInstrumentationVersion, convey.ShouldEqual, "2.6.1")
+		convey.So(EventInstrumentationVersion, convey.ShouldEqual, "2.6.2")
 	})
 }
